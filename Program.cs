7bee650b29564ebeeed297f261557ae47da68b24
Genival Junior <genivaljunior54321@gmail.com>
2023-12-27 21:23:23 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("TESTE NOKIA");
+
+Smartphone nokia = new Nokia (numero: "123", modelo: "Nokia G60 5G",imei: "111", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Twitch");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("TESTE IPHONE");
+
+Smartphone iphone = new Iphone (numero: "321", modelo: "Iphone 15 PRO",imei: "222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
